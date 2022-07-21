2c3236dad2a840c4efe4b2efe357d34e74f9ff71
MeetPancholi01 <85289669+MeetPancholi01@users.noreply.github.com>
2022-07-21 23:06:47 +0530

    Create curr_template.cpp

---
+++ curr_template.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+#include <bits/stdc++.h>
+#define ll long long
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+typedef tree <ll,null_type,greater_equal<ll>,rb_tree_tag,tree_order_statistics_node_update> oset; //ordered_set
+#define pb push_back
+
+
+int main(){
+
+    //code here
+    return 0;
+}
