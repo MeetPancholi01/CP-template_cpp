aee49ca9a925ec3ab3306c2e2e552bd2e3b3b8dd
MeetPancholi01 <85289669+MeetPancholi01@users.noreply.github.com>
2022-07-29 19:49:48 +0530

    Update curr_template.cpp

--- curr_template.cpp
+++ curr_template.cpp
@@ -7,7 +7,19 @@ using namespace std;
 using namespace __gnu_pbds;
 typedef tree <ll,null_type,greater_equal<ll>,rb_tree_tag,tree_order_statistics_node_update> oset; //ordered_set
 #define pb push_back
-
+ll fastExpo(ll base, ll expo, ll mod){
+    ll ans = 1;
+    while(expo > 0){
+        if(expo%2){
+            ans *= base;
+            ans %= mod;
+        }
+        base *= base;
+        base %= mod;
+        expo /= 2;
+    }
+    return ans;
+}
 
 int main(){
     ios_base::sync_with_stdio(false);
