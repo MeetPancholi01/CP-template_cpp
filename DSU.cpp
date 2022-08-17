1d16b8159af9269cac67b3f02043c0a4bc5757f9
MeetPancholi01 <85289669+MeetPancholi01@users.noreply.github.com>
2022-08-17 18:56:58 +0530

    Update DSU.cpp

--- DSU.cpp
+++ DSU.cpp
@@ -1,9 +1,9 @@
-vector<ll>size(1e5+10,0);
+vector<ll>siz(1e5+10,0);
 vector<ll>parent(1e5+10,-1);
 
 void make(ll v){
     parent[v] = v;
-    size[v] = 1;
+    siz[v] = 1;
 }
 
 ll find(ll v){
@@ -15,10 +15,10 @@ void Union(ll a, ll b){
     a = find(a);
     b = find(b);
     if(a != b){
-        if(size[a] < size[b]){
+        if(siz[a] < siz[b]){
             swap(a,b);
         }
         parent[b] = a;
-        size[a] += size[b];
+        siz[a] += siz[b];
     }
 }
