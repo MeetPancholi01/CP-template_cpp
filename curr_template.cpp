3524c2a812f8ce02ec572b38b555a4a78879d7d4
MeetPancholi01 <85289669+MeetPancholi01@users.noreply.github.com>
2022-08-23 13:20:15 +0530

    Update curr_template.cpp

--- curr_template.cpp
+++ curr_template.cpp
@@ -38,6 +38,10 @@ ll fact_mod(ll n,ll mod){
 }
 
 int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin); //file input.txt is opened in reading mode i.e "r"
+    freopen("output.txt","w",stdout);  //file output.txt is opened in writing mode i.e "w"
+    #endif
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     //code here
