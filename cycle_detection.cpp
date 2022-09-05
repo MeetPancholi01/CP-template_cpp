3e970b55b89d3f63772b8f290d93da678f84fdd4
MeetPancholi01 <85289669+MeetPancholi01@users.noreply.github.com>
2022-09-05 21:52:37 +0530

    Update cycle_detection.cpp

--- cycle_detection.cpp
+++ cycle_detection.cpp
@@ -1,3 +1,27 @@
+// FOR UNDIRECTED GRAPH
+vector<bool>vis(1e5,false);
+vector<int>ans;
+bool detect(int node,int prev,vector<vector<int>>&adj){
+    vis[node]=true;
+    ans.pb(node);
+    for(auto it:adj[node]){
+        if(!vis[it]){
+            if(detect(it,node,adj)){
+                return true;
+            }
+        }
+        else{
+            if(it != prev){
+                ans.pb(it);
+                return true;
+            }
+        }
+        // ans.pop_back();
+    }
+    ans.pop_back();
+    return false;
+}
+// FOR DIRECTED GRAPH
 vector<bool>vis(1e5,false);
 vector<int>ans;
 bool detect(int node,int prev,vector<vector<int>>&adj){
