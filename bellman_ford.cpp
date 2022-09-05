8133086f748eb850de72f5c384284df3be4801d8
MeetPancholi01 <85289669+MeetPancholi01@users.noreply.github.com>
2022-09-05 17:34:30 +0530

    Update bellman_ford.cpp

--- bellman_ford.cpp
+++ bellman_ford.cpp
@@ -6,11 +6,9 @@ struct triplet{
 	int second;
 	int third;
 };
- 
 int n, m;	
 vector<triplet> edges;
 vector<int> dist;
- 
 void bellman_ford()
 {
 	for(int i = 1; i < n; ++i)
@@ -70,4 +68,85 @@ int32_t main()
 		return 0;
 	} 
 	cout << dist[n] * (-1) << endl;
+}
+
+
+// BELLMAN FORD CAPTURING THE NEGATIVE CYCLE --------------------------------------------------------------------------------------------------------------
+struct triplet{
+    ll a;
+    ll b;
+    ll c;
+};
+
+ll inf = 1e17;
+
+vector<ll>dist;
+vector<ll>relaxant;
+vector<triplet>adj;
+ll x;
+void bellman_ford(ll n,ll m){
+    for(int i=1;i<=n;i++){
+        x = -1;
+        for(auto e:adj){
+            ll u = e.a;
+            ll v = e.b;
+            ll w = e.c;
+            if(dist[u]+w < dist[v]){
+                dist[v]=dist[u]+w;
+                relaxant[v] = u;
+                x = v;
+            }
+
+        }
+    }
+}
+
+
+
+int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin); //file input.txt is opened in reading mode i.e "r"
+    freopen("output.txt","w",stdout);  //file output.txt is opened in writing mode i.e "w"
+    #endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    //code here
+    ll n,m;
+    cin >> n >> m;
+    dist.resize(n+1);
+    adj.resize(m);
+    relaxant.resize(n+1);
+    for(int i=1;i<=n;i++){
+        dist[i] = inf;
+    }
+    struct triplet inp;
+    for(int i=0;i<m;i++){
+        cin >> inp.a >> inp.b>> inp.c;
+        adj[i] = inp;
+    }
+    bellman_ford(n,m);
+    if(x == -1){
+        cout << "NO" << endl;
+    }
+    else{
+        vector<ll>cycle;
+        for(int i=1;i<=n;i++){
+            x = relaxant[x];
+        }
+        for(ll v=x;;v=relaxant[v]){
+            cycle.pb(v);
+            if(v == x and cycle.size() > 1){
+                break;
+            }
+        }
+        reverse(cycle.begin(),cycle.end());
+        cout << "YES" << endl;
+        for(int i=0;i<cycle.size();i++){
+            cout << cycle[i] << " ";
+        }
+        cout << endl;
+    }
+
+    
+    return 0;
 }
