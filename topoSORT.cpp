be43eb03b983bc996a0e0c45823877832c4a570b
MeetPancholi01 <85289669+MeetPancholi01@users.noreply.github.com>
2022-09-06 18:09:20 +0530

    Update topoSORT.cpp

--- topoSORT.cpp
+++ topoSORT.cpp
@@ -19,3 +19,33 @@ while(!st.empty()){
     ans.pb(st.top());
     st.pop();
 }
+
+// TOPOSORT BY BFS----------------------------------------------------------------
+vector<ll> topoSort(ll N,vector<vector<ll>>adj){
+    queue<ll>q;
+    vector<ll>indeg(N+1,0);
+    for(int i=1;i<=N;i++){
+        for(auto it:adj[i]){
+            indeg[it]++;
+        }
+    }
+
+    for(int i=1;i<=N;i++){
+        if(indeg[i] == 0){
+            q.push(i);
+        }
+    }
+    vector<ll>topo;
+    while(!q.empty()){
+        ll node = q.front();
+        q.pop();
+        topo.pb(node);
+        for(auto it:adj[node]){
+            indeg[it]--;
+            if(indeg[it] == 0){
+                q.push(it);
+            }
+        }
+    }
+    return topo;
+}
