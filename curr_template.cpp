75e179823e1c67be346c35f780c1ffea65a67455
MeetPancholi01 <85289669+MeetPancholi01@users.noreply.github.com>
2022-09-18 20:33:12 +0530

    Update curr_template.cpp

--- curr_template.cpp
+++ curr_template.cpp
@@ -38,6 +38,18 @@ ll fact_mod(ll n,ll mod){
     return ans;
 }
 
+void print_arr(ll arr[],ll n){
+    for(int i=0;i<n;i++){
+        cout << arr[i] << " ";
+    }
+}
+
+void print_vec(vector<ll>&arr){
+    for(int i=0;i<arr.size();i++){
+        cout << arr[i] << " ";
+    }
+}
+
 int main(){
     #ifndef ONLINE_JUDGE
     freopen("input.txt","r",stdin); //file input.txt is opened in reading mode i.e "r"
